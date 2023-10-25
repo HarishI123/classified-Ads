42529328f188bf9f961e3ce6b128848d559eb473
Harish <116890370+HarishI123@users.noreply.github.com>
2023-10-25 21:09:51 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-
+python -m pip install --upgrade pip
 poetry install
-pip install --upgrade pip
+
 python manage.py collectstatic --no-input
 python manage.py migrate
